e8dcf9c49cb2f78ef978e21e2b2754b3f942a765
Dmytro Volkov <dmytro.volkov.p@gmail.com>
2022-09-03 19:08:24 +0300

    premake5 added

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "VEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "VEngine"
+	location "VEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"VE_PLATFORM_WINDOWS",
+			"VE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+		filter "configurations:Debug"
+			defines "VE_DEBUG"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "VE_RELEASE"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "VE_DIST"
+			optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"VEngine/vendor/spdlog/include",
+		"VEngine/src"
+	}
+
+	links
+	{
+		"VEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"VE_PLATFORM_WINDOWS"
+		}
+
+		filter "configurations:Debug"
+			defines "VE_DEBUG"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "VE_RELEASE"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "VE_DIST"
+			optimize "On"
